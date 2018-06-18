9c4704217d5a4215ab3013d6a5e17a741022bbc5
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-06-17 19:31:53 -0500

    Animated with panresponder

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ActivityIndicator, AsyncStorage, Text, View, Image, StyleSheet} from 'react-native'
+import {ActivityIndicator, AsyncStorage, Text, View, Image, StyleSheet, Animated, PanResponder} from 'react-native'
 import Video from 'react-native-video'
 
 import lights from '../lights.mp4'
@@ -15,18 +15,61 @@ class Splash extends React.Component {
     }
   }
 
+  componentWillMount () {
+    this.animatedValue = new Animated.ValueXY()
+    this._value = {x: 0, y: 0}
+    this.animatedValue.addListener((value) => this._value = value)
+    this.panResponder =new PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderGrant: (e, gestureState) => {
+        this.animatedValue.setOffset({
+          x: this._value.x,
+          y: this._value.y
+        })
+        this.animatedValue.setValue({x: 0, y: 0})
+      },
+      onPanResponderMove: Animated.event([
+        null, {dx: this.animatedValue.x, dy: this.animatedValue.y}
+      ]),
+      onPanResponderRelease: (e, gestureState) => {
+        this.animatedValue.flattenOffset()
+        Animated.decay(this.animatedValue, {
+          deceleration: 0.997,
+          velocity: {x: gestureState.vx, y: gestureState.vy}
+        }).start()
+      }
+    })
+  }
+
   render () {
-    return (<View style={{flex: 1,
-      flexDirection: 'column',
-      backgroundColor: 'white',
-      alignItems: 'center',
-      justifyContent: 'center'}}>
-      <Video
-        source={lights}
-        resizeMode='cover'
-        style={StyleSheet.absoluteFill} />
+    const animatedStyle = {
+      transform: this.animatedValue.getTranslateTransform()
+    }
+    return (<View style={styles.container}>
+      <Animated.View style={[styles.box, animatedStyle]} {...this.panResponder.panHandlers}>
+        <Text style={styles.text}>Hola</Text>
+      </Animated.View>
       </View>)
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: "#333",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#FFF",
+    fontSize: 20,
+  }
+})
 export default Splash
