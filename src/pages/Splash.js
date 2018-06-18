7048be21d77b6f975df964e6e2fdf657c2348e68
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-06-17 22:45:02 -0500

    Animated iterpolation

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ActivityIndicator, AsyncStorage, Text, View, Image, StyleSheet} from 'react-native'
+import {ActivityIndicator, AsyncStorage, Text, View, Image, StyleSheet, Animated} from 'react-native'
 import Video from 'react-native-video'
 
 import lights from '../lights.mp4'
@@ -15,18 +15,53 @@ class Splash extends React.Component {
     }
   }
 
+  componentWillMount () {
+    this.animatedValue = new Animated.Value(0)
+  }
+
+  componentDidMount () {
+    Animated.timing(this.animatedValue, {
+      toValue: 150,
+      duration: 1500
+    }).start()
+  }
+
   render () {
-    return (<View style={{flex: 1,
-      flexDirection: 'column',
-      backgroundColor: 'white',
-      alignItems: 'center',
-      justifyContent: 'center'}}>
-      <Video
-        source={lights}
-        resizeMode='cover'
-        style={StyleSheet.absoluteFill} />
+    const interpolateColor = this.animatedValue.interpolate({
+      inputRange: [0, 150],
+      outputRange: ['rgb(0, 0, 0)', 'gray']
+    })
+
+    const animatedStyle = {
+      backgroundColor: interpolateColor,
+      transform: [
+        {
+          translateY: this.animatedValue
+        }
+      ]
+    }
+    return (<View style={[styles.container]}>
+      <Animated.View style={[styles.box,animatedStyle]}/>
       </View>)
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: "#333",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#FFF",
+    fontSize: 20,
+  }
+})
 export default Splash
