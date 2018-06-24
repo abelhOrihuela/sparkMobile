aa687c4b0f0013c932a96d82aae1e8f493e4e22f
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-06-23 20:16:37 -0500

    Sequence of transitions

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ActivityIndicator, AsyncStorage, Text, View, Image, StyleSheet} from 'react-native'
+import {ActivityIndicator, AsyncStorage, Text, View, Image, StyleSheet, Animated} from 'react-native'
 import Video from 'react-native-video'
 
 import lights from '../lights.mp4'
@@ -15,18 +15,63 @@ class Splash extends React.Component {
     }
   }
 
+  componentWillMount () {
+    this.animatedValue1 = new Animated.Value(0)
+    this.animatedValue2 = new Animated.Value(0)
+  }
+
+  componentDidMount () {
+    Animated.sequence(
+      [
+        Animated.timing(this.animatedValue1, {
+          toValue: 150,
+          duration: 1000
+        }),
+        Animated.spring(this.animatedValue2, {
+          toValue: 3
+        }),
+        Animated.timing(this.animatedValue1, {
+          toValue: 0,
+          duration: 1000
+        }),
+        Animated.spring(this.animatedValue2, {
+          toValue: 0.5
+        })
+      ]
+    ).start()
+  }
+
   render () {
-    return (<View style={{flex: 1,
-      flexDirection: 'column',
-      backgroundColor: 'white',
-      alignItems: 'center',
-      justifyContent: 'center'}}>
-      <Video
-        source={lights}
-        resizeMode='cover'
-        style={StyleSheet.absoluteFill} />
-      </View>)
+
+    const animatesStyle = {
+      transform: [
+        {
+          translateY: this.animatedValue1
+        },
+        {
+          scale: this.animatedValue2
+        }
+      ]
+    }
+    return (
+      <View style={styles.container}>
+        <Animated.View style={[styles.box, animatesStyle]}/>
+      </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "#333"
+  }
+});
 
 export default Splash
