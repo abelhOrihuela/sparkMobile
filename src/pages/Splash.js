2b1ed6a356301fc8c9709a8eeedb2e1602dc9b2e
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-06-23 23:02:14 -0500

    feat(Splash.js): Parallel transition
    Parallel transition

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ActivityIndicator, AsyncStorage, Text, View, Image, StyleSheet} from 'react-native'
+import {ActivityIndicator, AsyncStorage, Text, View, Image, StyleSheet, Animated} from 'react-native'
 import Video from 'react-native-video'
 
 import lights from '../lights.mp4'
@@ -14,19 +14,49 @@ class Splash extends React.Component {
     this.state = {
     }
   }
+  componentWillMount () {
+    this.animatedValue1 = new Animated.Value(0)
+    this.animatedValue2 = new Animated.Value(1)
+  }
 
+  componentDidMount () {
+    Animated.parallel(
+      [
+        Animated.timing(this.animatedValue1, {
+          toValue: 500,
+          duration: 300
+        }),
+        Animated.spring(this.animatedValue2, {
+          toValue: 3
+        })
+      ]
+    ).start()
+  }
   render () {
-    return (<View style={{flex: 1,
-      flexDirection: 'column',
-      backgroundColor: 'white',
-      alignItems: 'center',
-      justifyContent: 'center'}}>
-      <Video
-        source={lights}
-        resizeMode='cover'
-        style={StyleSheet.absoluteFill} />
-      </View>)
+    const animatedStyles = {
+      transform: [
+        {translateY: this.animatedValue1},
+        {scale: this.animatedValue2}
+      ]
+    }
+    return (
+    <View style={[styles.container]}>
+      <Animated.View style={[styles.box, animatedStyles]} />
+    </View>
+      )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  box: {
+    backgroundColor: '#333',
+    width: 100,
+    height: 100,
+  }
+})
 
 export default Splash
