2ad368d6e6eab0bbcd0a4840bd78f35e611a7bc5
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-07-21 12:26:32 -0500

    feat(Form for profile): profile.js
    profile.js

--- src/stacks/drawer.js
+++ src/stacks/drawer.js
@@ -2,9 +2,11 @@ import {DrawerNavigator} from 'react-navigation'
 import Splash from '../pages/splash'
 import Dashboard from '../pages/dashboard'
 import Todo from '../pages/todo'
+import Profile from '../pages/profile'
 
 const Drawer = DrawerNavigator({
   Splash: {screen: Splash},
+  Profile: {screen: Profile},
   Todo: {screen: Todo},
   Dashboard: {screen: Dashboard}
 
