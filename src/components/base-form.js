421ad3d8330cd55b4068460881b26ad9aaaa024e
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-07-22 16:43:19 -0500

    feat(base-form.js): Added base form

---
+++ src/components/base-form.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableHighlight
+} from 'react-native'
+import t from 'tcomb-form-native'
+const Form = t.form.Form
+
+const stylesheet = {...t.form.Form.stylesheet}
+
+stylesheet.textbox.normal.borderWidth = 0
+stylesheet.textbox.error.borderWidth = 0
+stylesheet.textbox.normal.marginBottom = 0
+stylesheet.textbox.error.marginBottom = 0
+
+stylesheet.textboxView.normal.borderWidth = 0
+stylesheet.textboxView.error.borderWidth = 0
+stylesheet.textboxView.normal.borderRadius = 0
+stylesheet.textboxView.error.borderRadius = 0
+stylesheet.textboxView.normal.borderBottomWidth = 1
+stylesheet.textboxView.error.borderBottomWidth = 1
+stylesheet.textbox.normal.marginBottom = 5
+stylesheet.textbox.error.marginBottom = 5
+
+class BaseForm extends Component {
+  onPress = (e) => {
+    const value = this._form.getValue();
+    console.log('value', value);
+
+    this.props.onSuccess(e)
+
+  }
+
+  onChange = (e) => {
+    this.props.onChange(e)
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Form type={this.props.type}
+          ref={c => (this._form = c)}
+          options={{...this.props.options, ...stylesheet}}
+          value={this.props.value}
+          onChange={this.props.onChange} />
+          { 
+            <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
+              <Text style={styles.buttonText}>Save</Text>
+            </TouchableHighlight>
+            
+          }
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  button: {
+    height: 36,
+    backgroundColor: '#48BBEC',
+    borderColor: '#48BBEC',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
+})
+
+
+BaseForm.propTypes = {
+  type: PropTypes.object.isRequired,
+  value: PropTypes.object.isRequired,
+  options: PropTypes.object,
+  onChange: PropTypes.func.isRequired,
+  onSuccess: PropTypes.func.isRequired,
+}
+
+export default BaseForm
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,6 +1,5 @@
-'use strict';
-
 import React, { Component } from 'react';
+import BaseForm from '../components/base-form'
 
 import {
   StyleSheet,
@@ -9,29 +8,13 @@ import {
   TextInput,
   ScrollView,
   TouchableHighlight
-} from 'react-native';
-import t from 'tcomb-form-native';
-const Form = t.form.Form;
-
-const stylesheet = {...t.form.Form.stylesheet}
-
-stylesheet.textbox.normal.borderWidth = 0;
-stylesheet.textbox.error.borderWidth = 0;
-stylesheet.textbox.normal.marginBottom = 0;
-stylesheet.textbox.error.marginBottom = 0;
-
-stylesheet.textboxView.normal.borderWidth = 0;
-stylesheet.textboxView.error.borderWidth = 0;
-stylesheet.textboxView.normal.borderRadius = 0;
-stylesheet.textboxView.error.borderRadius = 0;
-stylesheet.textboxView.normal.borderBottomWidth = 1;
-stylesheet.textboxView.error.borderBottomWidth = 1;
-stylesheet.textbox.normal.marginBottom = 5;
-stylesheet.textbox.error.marginBottom = 5;
+} from 'react-native'
+import t from 'tcomb-form-native'
+const Form = t.form.Form
 
 class Profile extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       value: {
         email: 'abel@happy.com',
@@ -39,7 +22,6 @@ class Profile extends Component {
         telephone: '7224666201'
       },
       options:{
-        stylesheet: stylesheet,
         fields: {
           email: {
             editable: false,
@@ -52,24 +34,21 @@ class Profile extends Component {
           }
         }
       }
-    };
+    }
   }
 
-  onChange = (e) => {
-    /*
-    console.log('e', e);
-    const value = this._form.getValue();
-    console.log('value: ', this._form);
-    */
+  handleOnChange = (e) => {
     this.setState({
       value: e
     })
   }
 
+  handleSucces = (e) => {
+  
+  }
+
   onPress = () => {
     const value = this._form.getValue();
-    console.log('value', value);
-
   }
 
   render() {
@@ -78,21 +57,21 @@ class Profile extends Component {
       email: t.String,
       telephone: t.String,
       address: t.maybe(t.String)
-    });
+    })
+
     return (
       <View style={styles.container}>
         <ScrollView>
-        <Form type={User}
-          ref={c => this._form = c}
-          options={this.state.options}
-          value={this.state.value}
-          onChange={this.onChange}/>
-          <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
-          <Text style={styles.buttonText}>Save</Text>
-        </TouchableHighlight>
+          <BaseForm
+            type={User}
+            options={this.state.options}
+            value={this.state.value}
+            onChange={this.handleOnChange}
+            onSuccess={this.handleSucces}
+            />
         </ScrollView>
       </View>
-    );
+    )
   }
 }
 
@@ -100,25 +79,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    // marginTop: 50,
     padding: 20,
     backgroundColor: '#FFF',
-  },
-  buttonText: {
-    fontSize: 18,
-    color: 'white',
-    alignSelf: 'center'
-  },
-  button: {
-    height: 36,
-    backgroundColor: '#48BBEC',
-    borderColor: '#48BBEC',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-    alignSelf: 'stretch',
-    justifyContent: 'center'
   }
-});
+})
 
-export default Profile;
+export default Profile
