f5a58d7ed8ca34e336f85b6b798ce4565cf6676e
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-07-23 23:36:23 -0500

    feat(stackNavigator.js): Fixed stacks
    Fixed stacks

--- src/stacks/drawer.js
+++ src/stacks/drawer.js
@@ -1,22 +1,14 @@
 import {DrawerNavigator} from 'react-navigation'
-import Login from '../pages/login'
-import Splash from '../pages/splash'
 import Dashboard from '../pages/dashboard'
 import Todo from '../pages/todo'
 import Profile from '../pages/profile'
 
 const Drawer = DrawerNavigator({
-  Splash: {screen: Splash},
-  Login: {screen: Login},
   Profile: {screen: Profile},
   Todo: {screen: Todo},
   Dashboard: {screen: Dashboard}
-
 }, {
-  headerMode: 'none',
-  navigationOptions: {
-    gesturesEnabled: false
-  }
+  headerMode: 'none'
 })
 
 export default Drawer
