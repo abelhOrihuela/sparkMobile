5ea9a8f52aa2802aa2ba8a2d6bb1b917018d42e3
Abel Orihuela <abelorihuelamendoza@hotmail.com>
2018-10-10 09:08:56 -0500

    Intro

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,14 +3,18 @@ import {
   View,
   StyleSheet,
   Modal,
+  Image,
   Text,
-  TouchableHighlight,
-  ActivityIndicator
+  TouchableOpacity,
+  ActivityIndicator,
+  Dimensions
 } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import VideoContainer from '../components/video'
 import ModalContainer from '../components/modal'
 import api from '../core/api'
+import logo from '../logoWhite.png'
+const {height, width} = Dimensions.get('window')
 
 export default class Dashboard extends Component {
   static navigationOptions = {
@@ -23,12 +27,14 @@ export default class Dashboard extends Component {
       return 'Home'
     }
   }
+
   constructor (props){
     super(props)
     this.state = {
       order: null,
-      modalVisible: false,
-      loading: true
+      modalIntro: false,
+      modalStatus: false,
+      loading: false
     }
   }
 
@@ -49,43 +55,72 @@ export default class Dashboard extends Component {
 
   loadView () {
     let {order} = this.state
-    console.log('order ===>', order);
     if (order.status !== 'delivered') {
-      console.log('Show modal intro');
       this.setState({
         loading: false,
-        modalVisible: false
+        modalIntro: true
       })
     } else {
       this.setState({
         loading: false
       })
-      console.log('Redirect home');
     }
   }
   setModalVisible(visible) {
-    this.setState({modalVisible: visible});
+    this.setState({
+      modalIntro: visible,
+      modalStatus: true
+    })
   }
 
   render () {
-    let {loading, modalVisible} = this.state
+    let {loading, modalIntro, modalStatus} = this.state
 
-    let content = (<ModalContainer
-      visible={modalVisible}
-      hideModal={(e) => this.setModalVisible(e)}>
-      <View style={styles.section}>
-        <Text>Bienvenido a</Text>
+    let modalOne = (<ModalContainer
+      visible={modalIntro}>
+      <View style={[styles.section, styles.doubleMarginTopBottom]}>
+        <Text style={styles.fontWhite18}>Bienvenido a</Text>
       </View>
+      <View style={[styles.section, styles.doubleMarginTopBottom]}>
+        <Image source={logo} />
+      </View>
+      <View style={[styles.section, styles.doubleMarginTopBottom]}>
+        <Text style={styles.fontWhite16}>Bienvenido a AlcheMycode, este será tu espacio personal para conocerte y poder cambiar tu vida.</Text>
+      </View>
+      <TouchableOpacity
+        style={[styles.btn, styles.doubleMarginTopBottom]}
+        onPress={() => this.setModalVisible(!modalIntro)}>
+        <Text style={styles.fontWhite16}>Continuar</Text>
+      </TouchableOpacity>
+    </ModalContainer>)
+
+    let modalTwo = (<ModalContainer
+      visible={modalStatus}>
+      <View style={[styles.section, styles.doubleMarginTopBottom]}>
+        <Image source={logo} />
+      </View>
+      <View style={[styles.section, styles.doubleMarginTopBottom]}>
+        <Text style={styles.fontWhite18}>Estamos preparando tus resultados.</Text>
+      </View>
+
+      <View style={[styles.section, styles.doubleMarginTopBottom]}>
+        <Text style={styles.fontWhite16}>¡Pronto iniciarás un nuevo estilo de vida!</Text>
+      </View>
+      <TouchableOpacity
+        style={[styles.btn, styles.doubleMarginTopBottom]}
+        onPress={() => this.setState({modalStatus: false})}>
+        <Text style={styles.fontWhite16}>Status</Text>
+      </TouchableOpacity>
     </ModalContainer>)
 
     if (loading) {
       return <ActivityIndicator />
     }
 
-
     return (
-      <View style={styles.container}>
-        {content}
+      <View>
+        {modalOne}
+        {modalTwo}
       </View>
     )
   }
@@ -93,11 +128,53 @@ export default class Dashboard extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'transparent'
+  },
+  background: {
+    width: width,
+    height: height
   },
   section: {
     justifyContent: 'center',
     flexDirection: 'row',
     margin: 10
+  },
+  imagePeople: {
+    width: width-80,
+    height: 230
+  },
+  fontBlue16: {
+    color: '#2771A2',
+    fontSize: 16
+  },
+  fontWhite18: {
+    color: '#FFFFFF',
+    fontSize: 18
+  },
+  fontWhite16: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    textAlign: 'center'
+  },
+  fontBlack14: {
+    color: '#000000',
+    fontSize: 14
+  },
+  btn: {
+    borderWidth: 1,
+    borderColor: '#FFFFFF',
+    padding: 10,
+    alignItems: 'center',
+    margin: 10
+  },
+  doubleMarginTopBottom: {
+    marginTop: 20,
+    marginBottom: 20
+  },
+  status: {
+    backgroundColor: '#2771A2',
   }
 })
